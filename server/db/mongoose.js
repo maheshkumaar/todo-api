4c54985555eb16614110033505da124987fbf013
Maheshkumaar <maheshkumaar2106@gmail.com>
2017-05-30 14:51:04 +0530

    Again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var localhost_uri = "mongodb://localhost:27017/TodoApp";
-var mongodb_uri = process.env.PROD_MONGODB || localhost_uri; 
-mongoose.connect(mongodb_uri);
+//var localhost_uri = "mongodb://localhost:27017/TodoApp";
+//var mongodb_uri = localhost_uri; 
+//mongoose.connect(mongodb_uri);
+
+mongoose.connect("mongodb://<mahesh_kumaar>:<cryptic123>@ds157631.mlab.com:57631/todo-api");
 
 module.exports = {mongoose};
