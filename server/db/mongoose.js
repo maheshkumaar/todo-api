942634c700f79cdca3866baa05e4862d8d275103
Maheshkumaar <maheshkumaar2106@gmail.com>
2017-05-30 14:39:49 +0530

    Mongo DB uri change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 var localhost_uri = "mongodb://localhost:27017/TodoApp";
-var mongodb_uri = process.env.MONGODB_URI || localhost_uri; 
+var mongodb_uri = process.env.PROD_MONGODB || localhost_uri; 
 mongoose.connect(mongodb_uri);
 
 module.exports = {mongoose};
